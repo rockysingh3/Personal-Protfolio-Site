ed8b7b55fb28c706e99e2a5d5fe2619fb131909a
Rocky Singh <rockygsingh@icloud.com>
2020-08-29 19:26:50 -0400

    updated links for git and livedemo

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,121 +1,150 @@
-import React, { Component } from 'react'
-import { Tabs, Tab, Grid, Cell, Card, CardTitle,  CardActions, Button, CardMenu, IconButton, CardText } from 'react-mdl'
-
+import React, { Component } from "react";
+import {
+  Tabs,
+  Tab,
+  Grid,
+  Cell,
+  Card,
+  CardTitle,
+  CardActions,
+  Button,
+  CardMenu,
+  IconButton,
+  CardText,
+} from "react-mdl";
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = { activeTab: 0 }
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div className="projects-grid">
+          {/* Project 1 */}
+
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "black",
+                height: "176px",
+                background:
+                  "url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png) center / cover",
+              }}
+            >
+              Calculator App
+            </CardTitle>
+
+            <CardText>sh;hfihsdiohfoishshdd nlsjildjl hsdF</CardText>
+            <CardActions border>
+              <a href="https://github.com/rockysingh3/React-Calculator">
+                <Button colored>GitHub</Button>
+              </a>
+              <a href="https://react-calcculator.herokuapp.com/">
+                <Button colored>LiveDemo</Button>
+              </a>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "black",
+                height: "176px",
+                background:
+                  "url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png) center / cover",
+              }}
+            >
+              BookList App
+            </CardTitle>
+
+            <CardText>sh;hfihsdiohfoishshdd nlsjildjl hsd</CardText>
+            <CardActions border>
+              <a href="https://github.com/rockysingh3/BookList">
+                <Button colored>GitHub</Button>
+              </a>
+
+              <a href="https://stark-spire-18890.herokuapp.com/">
+                <Button colored>LiveDemo</Button>
+              </a>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 3 */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{
+                color: "black",
+                height: "176px",
+                background:
+                  "url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png) center / cover",
+              }}
+            >
+              WatchList Movie App
+            </CardTitle>
+
+            <CardText>sh;hfihsdiohfoishshdd nlsjildjl hsd</CardText>
+            <CardActions border>
+              <a href="https://github.com/rockysingh3/WatchList">
+                <Button colored>GitHub</Button>
+              </a>
+              <Button colored>LiveDemo</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1>This is Angular</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h1>This is VueJS</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 3) {
+      return (
+        <div>
+          <h1>This is MongoDB</h1>
+        </div>
+      );
     }
+  }
 
-    toggleCategories() {
-        if(this.state.activeTab === 0) {
-            return (
+  render() {
+    return (
+      <div className="category-tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={(tabId) => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+        </Tabs>
 
-                <div className="projects-grid">
-
-                    { /* Project 1 */ }
-
-<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color: 'black', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png) center / cover'}}>Calculator App</CardTitle>
-                
-                <CardText>
-                    sh;hfihsdiohfoishshdd
-                    nlsjildjl
-                    hsdF
-                </CardText>
-                <CardActions border>
-                    <a href="https://github.com/rockysingh3/React-Calculator">
-                    <Button colored>GitHub</Button>
-                    </a>
-                    <a href="https://react-calcculator.herokuapp.com/">
-                    <Button colored>LiveDemo</Button>
-                    </a>
-                </CardActions>
-                <CardMenu style={{color: '#fff'}}>
-                    <IconButton name="share" />
-                </CardMenu>
-                </Card>
-
-                { /* Project 2 */ }
-                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color: 'black', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png) center / cover'}}>React Project 2</CardTitle>
-
-                    <CardText>
-                    sh;hfihsdiohfoishshdd
-                    nlsjildjl
-                    hsd
-                    </CardText>
-                    <CardActions border>
-                        <Button colored>GitHub</Button>
-                        
-                        <Button colored>LiveDemo</Button>
-                    </CardActions>
-                    <CardMenu style={{color: '#fff'}}>
-                        <IconButton name="share" />
-                    </CardMenu>
-                </Card>
-
-                { /* Project 3 */ }
-                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color: 'black', height: '176px', background: 'url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png) center / cover'}}>React Project 3</CardTitle>
-
-                    <CardText>
-                    sh;hfihsdiohfoishshdd
-                    nlsjildjl
-                    hsd
-                    </CardText>
-                    <CardActions border>
-                        <Button colored>GitHub</Button>
-                        <Button colored>CodePen</Button>
-                        <Button colored>LiveDemo</Button>
-                    </CardActions>
-                    <CardMenu style={{color: '#fff'}}>
-                        <IconButton name="share" />
-                    </CardMenu>
-                </Card>
-
-
-                </div>
-                
-                
-            )
-        }else if(this.state.activeTab === 1) {
-            return (
-                <div><h1>This is Angular</h1></div>
-            )
-        }else if(this.state.activeTab === 2) {
-            return (
-                <div><h1>This is VueJS</h1></div>
-            )
-        }else if(this.state.activeTab === 3) {
-            return (
-                <div><h1>This is MongoDB</h1></div>
-            )
-        }
-    }
-
-
-    render() {
-        return (
-            <div className="category-tabs">
-                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-                    <Tab>React</Tab>
-                </Tabs>
-
-                
-                    <Grid >
-                        <Cell col={12}>
-                            <div className="content">{this.toggleCategories()}</div>
-                        </Cell>
-                    </Grid>
-                
-
-            </div>
-        )
-    }
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
 }
-
 
 export default Projects;
