0cceb0226a271c943860191eae1754d34994c127
Rocky Singh <rockygsingh@icloud.com>
2020-08-31 10:42:28 -0400

    made the navbar mobile resposive

--- src/App.js
+++ src/App.js
@@ -1,37 +1,61 @@
-import React from 'react';
-import './App.css';
-import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
-import Main from './components/main'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./App.css";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import Main from "./components/main";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
+    //     <div className="demo-big-content">
+    //     <Layout>
+    //         <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">My Protfolio</Link>}  scroll>
+    //             <Navigation>
 
-    <div className="demo-big-content">
-    <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">My Protfolio</Link>}  scroll>
-            <Navigation>
-                
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
+    //                 <Link to="/resume">Resume</Link>
+    //                 <Link to="/aboutme">About Me</Link>
+    //                 <Link to="/projects">Projects</Link>
+    //                 <Link to="/contact">Contact</Link>
+    //             </Navigation>
+    //         </Header>
+    //         <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">My Protfolio</Link>}>
+    //             <Navigation>
+    //                 <Link to="/resume">Resume</Link>
+    //                 <Link to="/aboutme">About Me</Link>
+    //                 <Link to="/projects">Projects</Link>
+    //                 <Link to="/contact">Contact</Link>
+    //             </Navigation>
+    //         </Drawer>
+    //         <Content>
+    //             <div className="page-content" />
+    //             <Main />
+    //         </Content>
+    //     </Layout>
+    // </div>
+
+    <div style={{ height: "300px", position: "relative" }} className="demo-big-content">
+      <Layout fixedHeader>
+        <Header
+        className="header-color"
+          title={
+            <span>
+              <span style={{ color: "#ddd" }}></span>
+              <strong>Rocky Singh's Protfolio</strong>
+            </span>
+          }
+        >
+          <Navigation>
+            <Link to="/resume">Resume</Link>
+            <Link to="/aboutme">About Me</Link>
+            <Link to="/projects">Projects</Link>
+            <Link to="/contact">Contact</Link>
+          </Navigation>
         </Header>
-        <Drawer title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">My Protfolio</Link>}>
-            <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/contact">Contact</Link>
-            </Navigation>
-        </Drawer>
         <Content>
-            <div className="page-content" />
-            <Main />
+          <div className="page-content" />
+          <Main />
         </Content>
-    </Layout>
-</div>
+      </Layout>
+    </div>
   );
 }
 
